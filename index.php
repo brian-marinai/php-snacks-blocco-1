221d5fea42fe64868361fc569a6db8189738716f
Brian Marinai <brianmarinai.bm@gmail.com>
2020-09-02 16:58:20 +0200

    esercizio in corso

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+
+  $arr = [
+    'squadraCasa' => ['Miami Heat', 'Boston Celtics', 'Chicago Bulls'],
+    'scuadraOspite' => ['L.A Lakers', 'Utah Jazz', 'Dallas Mavericks'],
+    'puntiCasa' => [101, 110, 98],
+    'puntiOspite' => [105, 115, 95]
+  ];
+
+  // var_dump($arr['squadraCasa'])
+
+  // print_r($arr['squadraCasa'][1], ['squadraCasa'][2] );
+
+  echo '<pre>'; print_r($arr); echo '</pre>';
+
+
+?>
