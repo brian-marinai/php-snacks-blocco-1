773f21704b02b64182ffb0ed90833f66d4778216
Brian Marinai <brianmarinai.bm@gmail.com>
2020-09-03 15:24:09 +0200

    esercizio corretto in classe

--- index.php
+++ index.php
@@ -1,18 +1,74 @@
+<!-- jsnack 1 -->
+<?php
+
+  $partite = [
+  [
+    'squadraCasa' => 'Miami Heat',
+    'squadraOspite' => 'L.A Lakers',
+    'puntiCasa' => 101,
+    'puntiOspite' => 105
+  ],
+  [
+    'squadraCasa' => 'Boston Celtics' ,
+    'squadraOspite' => 'Utah Jazz',
+    'puntiCasa' => 110,
+    'puntiOspite' => 120
+  ],
+  [
+
+    'squadraCasa' => 'Chicago Bulls',
+    'squadraOspite' => 'Dallas Mavericks',
+    'puntiCasa' => 98,
+    'puntiOspite' => 90
+  ],
+];
+
+  foreach ($partite as $m) {
+
+    echo $m["squadraCasa"] . ' - ' . $m['squadraOspite'] . ' | ' . $m['puntiCasa'] . ' - ' . $m['puntiOspite'] . '<br>';
+  }
+
+?>
+
+
+<!-- jsnack 2 -->
 
 <?php
 
-  $arr = [
-    'squadraCasa' => ['Miami Heat', 'Boston Celtics', 'Chicago Bulls'],
-    'scuadraOspite' => ['L.A Lakers', 'Utah Jazz', 'Dallas Mavericks'],
-    'puntiCasa' => [101, 110, 98],
-    'puntiOspite' => [105, 115, 95]
-  ];
+  $nome = $_GET['name'];
+  $email = $_GET['mail'];
+  $age = $_GET['age'];
 
-  // var_dump($arr['squadraCasa'])
+  $validname = strlen($name) > 3;
+  $validmail = strpos($email, '.') !== false && strpos($email, '@') !== false;
+  $validage = is_numeric($age);
 
-  // print_r($arr['squadraCasa'][1], ['squadraCasa'][2] );
-
-  echo '<pre>'; print_r($arr); echo '</pre>';
-
+  if ($validname && $validmail && $validage) {
+    echo 'Accesso Riuscito';
+  } else {
+    echo 'Accesso Negato';
+  }
 
 ?>
+
+<!-- jsnack 3 -->
+
+
+<?php
+
+  $numeri = [];
+
+  while(count($numeri) < 15) {
+
+    $numerir = rand(1, 100);
+
+    if (!in_array($numerir, $numeri)) {
+
+      $numeri[] = $numerir;
+
+    }
+  }
+
+  var_dump($numeri);
+
+ ?>
